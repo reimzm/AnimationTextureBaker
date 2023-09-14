e244c74a165893c379254cdd643743bb25eeeb3f
Nukadelic <6582633+nukadelic@users.noreply.github.com>
2023-09-14 18:32:39 +0300

    improved and converted to a upm package

---
+++ Baker/AnimationBaker.cs
@@ -0,0 +1,325 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+#endif
+
+public class AnimationBaker : MonoBehaviour
+{
+
+    public ComputeShader infoTexGen;
+    public Shader playShader;
+    public AnimationClip[] clips;
+
+    public struct VertInfo
+    {
+        public Vector3 position;
+        public Vector3 normal;
+        public Vector3 tangent;
+    }
+
+    private void Scan()
+    {
+        var animation = GetComponent<Animation>();
+        var animator = GetComponent<Animator>();
+
+        if (animation != null)
+        {
+            clips = new AnimationClip[animation.GetClipCount()];
+            var i = 0;
+            foreach (AnimationState state in animation)
+                clips[i++] = state.clip;
+        }
+        else if (animator != null)
+            clips = animator.runtimeAnimatorController.animationClips;
+    }
+
+    // public enum BakeFrameResolution { F6 = 6, F12 = 12, F20 = 20, F24 = 24, F40 = 40, F60 = 60, F120 = 120 }
+    // public BakeFrameResolution FrameResolution = BakeFrameResolution.F20;
+
+    [Range(2,12)] public int FrameResolution = 4;
+
+    public string saveToFolder = "AnimationBakerTextures";
+
+    public bool createPrefabs = false;
+
+    public bool createMeshAsset = false;
+    public bool optimizeMeshOnSave = false;
+    [Tooltip("Make it a bit harder to reverse engenieer this model")]
+    public bool collpaseMesh = false;
+
+    int GetFrameCount( AnimationClip clip )
+    {
+        return Mathf.NextPowerOfTwo( ( int ) ( clip.length * ( ( int ) FrameResolution ) ) );
+    }
+
+    // Convert render texture to texture 2d 
+    public static Texture2D ConvertRT( RenderTexture rt )
+    {
+        TextureFormat format;
+
+        switch (rt.format)
+        {
+            case RenderTextureFormat.ARGBFloat:
+                format = TextureFormat.RGBAFloat;
+                break;
+            case RenderTextureFormat.ARGBHalf:
+                format = TextureFormat.RGBAHalf;
+                break;
+            case RenderTextureFormat.ARGBInt:
+                format = TextureFormat.RGBA32;
+                break;
+            case RenderTextureFormat.ARGB32:
+                format = TextureFormat.ARGB32;
+                break;
+            default:
+                format = TextureFormat.ARGB32;
+                Debug.LogWarning("Unsuported RenderTextureFormat.");
+                break;
+        }
+
+        return Convert(rt, format);
+    }
+
+    static Texture2D Convert(RenderTexture rt, TextureFormat format)
+    {
+        var tex2d = new Texture2D(rt.width, rt.height, format, false);
+        var rect = Rect.MinMaxRect(0f, 0f, tex2d.width, tex2d.height);
+        RenderTexture.active = rt;
+        tex2d.ReadPixels(rect, 0, 0);
+        RenderTexture.active = null;
+        return tex2d;
+    }
+
+
+
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(AnimationBaker))] class Inspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Space(10);
+
+            var script = (AnimationBaker) target;
+
+            var s = "";
+
+            if( script.clips == null || script.clips.Length < 1 )
+            {
+                if( GUILayout.Button("Scan") )
+                {
+                    script.Scan();
+                }
+
+                return;
+            }
+
+            foreach (var clip in script.clips)
+            {
+                var frames = script.GetFrameCount( clip );
+
+                s += $"{clip.name}: {frames}\n";
+            }
+
+            s = "Frame counts per clip: \n" + s;
+
+            EditorGUILayout.HelpBox( new GUIContent( s ) );
+
+            if( GUILayout.Button("Bake Textures") )
+            {
+                script.Bake();
+            }
+        }
+    }
+
+    string FixPath( string s )
+    {
+        //return s.Replace('|', '_').Replace('/', '_').Replace('\\', '|').Replace(' ', '_')
+        //    .Replace(':', '-').Replace('*', '-').Replace('?', '-')
+        //    .Replace('<', '[').Replace('>', ']');
+
+        //string validPath = Regex.Replace( s , @"[<>:""|?*]", "_");
+        //validPath = Regex.Replace(validPath, @"\s+", " ");
+
+        if ( string.IsNullOrEmpty( s ) ) throw new System.Exception("EMPTY");
+
+        // Replace invalid characters with an underscore
+        string invalidChars = new string(Path.GetInvalidPathChars());
+        string validPath = Regex.Replace( s , $"[{Regex.Escape(invalidChars)}]", "_" );
+        // Combine multiple path separators into a single one
+        string separator = Path.DirectorySeparatorChar.ToString();
+        string doubleSeparator = separator + separator;
+        while (validPath.Contains(doubleSeparator))
+        {
+            validPath = validPath.Replace(doubleSeparator, separator);
+        }
+        
+        return validPath.Trim();
+    }
+
+    [ContextMenu("bake texture")]
+    void Bake()
+    {
+        var skin = GetComponentInChildren<SkinnedMeshRenderer>();
+        var defaultMesh = skin.sharedMesh;
+        var vCount = skin.sharedMesh.vertexCount;
+        var texWidth = Mathf.NextPowerOfTwo(vCount);
+        var mesh = new Mesh();
+
+        var folderName = FixPath(saveToFolder);
+        var folderPath = Path.Combine("Assets", folderName);
+        if (!AssetDatabase.IsValidFolder(folderPath))
+            AssetDatabase.CreateFolder("Assets", folderName);
+
+        var subFolder = name;
+        var subFolderPath = Path.Combine(folderPath, subFolder);
+        if (!AssetDatabase.IsValidFolder(subFolderPath))
+            AssetDatabase.CreateFolder(folderPath, subFolder);
+
+        if (createMeshAsset)
+        {
+            var meshAssetPath = Path.Combine(subFolderPath, $"{FixPath(name)}.mesh" + ".asset");
+            defaultMesh = Instantiate(defaultMesh);
+            if (!collpaseMesh && optimizeMeshOnSave) MeshUtility.Optimize(defaultMesh);
+            if(collpaseMesh) defaultMesh.SetVertices( new Vector3[ defaultMesh.vertexCount ] );
+            AssetDatabase.CreateAsset( defaultMesh, meshAssetPath );
+            AssetDatabase.SaveAssets();
+            defaultMesh = AssetDatabase.LoadAssetAtPath<Mesh>( meshAssetPath );
+        }
+
+        int clip_index = -1;
+
+        foreach (var clip in clips)
+        {
+            clip_index ++ ;
+
+            var frames = GetFrameCount( clip );
+
+            var dt = clip.length / frames;
+            var infoList = new List<VertInfo>();
+
+            // var frame_name = GetFrameResName();
+
+            var pRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            pRt.name = $"{name}.textPos.{clip.name}.{frames}F";
+            var nRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            nRt.name = $"{name}.texNorm.{clip.name}.{frames}F";
+            var tRt = new RenderTexture(texWidth, frames, 0, RenderTextureFormat.ARGBHalf);
+            tRt.name = $"{name}.texTan.{clip.name}.{frames}F";
+            foreach (var rt in new[] { pRt, nRt, tRt })
+            {
+                rt.enableRandomWrite = true;
+                rt.Create();
+                RenderTexture.active = rt;
+                GL.Clear(true, true, Color.clear);
+            }
+
+            for (var i = 0; i < frames; i++)
+            {
+                clip.SampleAnimation(gameObject, dt * i);
+                skin.BakeMesh(mesh);
+
+                var verexArry = mesh.vertices;
+                var normalArry = mesh.normals;
+                var tangentArray = mesh.tangents;
+                infoList.AddRange(Enumerable.Range(0, vCount)
+                    .Select(idx => new VertInfo()
+                    {
+                        position = verexArry[idx],
+                        normal = normalArry[idx],
+                        tangent = tangentArray[idx],
+                    })
+                );
+            }
+            var buffer = new ComputeBuffer(infoList.Count, System.Runtime.InteropServices.Marshal.SizeOf(typeof(VertInfo)));
+            buffer.SetData(infoList.ToArray());
+
+            var kernel = infoTexGen.FindKernel("CSMain");
+
+            infoTexGen.GetKernelThreadGroupSizes(kernel, out uint x, out uint y, out uint z);
+
+            infoTexGen.SetInt("VertCount", vCount);
+            infoTexGen.SetBuffer(kernel, "Info", buffer);
+            infoTexGen.SetTexture(kernel, "OutPosition", pRt);
+            infoTexGen.SetTexture(kernel, "OutNormal", nRt);
+            infoTexGen.SetTexture(kernel, "OutTangent", tRt);
+            infoTexGen.Dispatch(kernel, vCount / (int)x + 1, frames / (int)y + 1, 1);
+
+            buffer.Release();
+
+            var posTex = ConvertRT(pRt);
+            var normTex = ConvertRT(nRt);
+            var tanTex = ConvertRT(tRt);
+
+            Graphics.CopyTexture(pRt, posTex);
+            Graphics.CopyTexture(nRt, normTex);
+            Graphics.CopyTexture(tRt, tanTex);
+
+            var mat = new Material( playShader );
+
+            mat.SetTexture("_MainTex", skin.sharedMaterial.mainTexture );
+            mat.SetTexture("_PosTex", posTex);
+            mat.SetTexture("_NmlTex", normTex);
+
+            //mat.SetFloat("_Length", clip.length);
+            //
+            //if (clip.wrapMode == WrapMode.Loop)
+            //{
+            //    mat.SetFloat("_Loop", 1f);
+            //    mat.EnableKeyword("ANIM_LOOP");
+            //}
+
+            var pRt_path = Path.Combine(subFolderPath,FixPath(pRt.name) + ".asset");
+            var nRt_path = Path.Combine(subFolderPath,FixPath(nRt.name) + ".asset");
+            var tRt_path = Path.Combine(subFolderPath,FixPath(tRt.name) + ".asset");
+
+            AssetDatabase.CreateAsset(posTex,   pRt_path);
+            AssetDatabase.CreateAsset(normTex,  nRt_path);
+            AssetDatabase.CreateAsset(tanTex,   tRt_path);
+
+            var mat_path = Path.Combine( subFolderPath, $"{FixPath(name)}.mat.{FixPath(clip.name)}.{frames}F.asset");
+
+            AssetDatabase.CreateAsset(mat, mat_path );
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            if ( createPrefabs )
+            {
+                var go = new GameObject(name + "." + clip.name);
+
+                go.transform.position = transform.position + Vector3.left * ( clip_index + 1 );
+                
+                go.AddComponent<MeshRenderer>().sharedMaterial = mat;
+                go.AddComponent<MeshFilter>().sharedMesh = defaultMesh;
+            
+                PrefabUtility.SaveAsPrefabAssetAndConnect( go, Path.Combine( folderPath, FixPath( go.name ) + ".prefab") , InteractionMode.AutomatedAction );
+            }
+
+            Selection.activeObject = mat;
+        }
+
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+    }
+
+    // Convert asset files to png 
+
+    [MenuItem("Custom/tex2png")]
+    public static void SaveSelection()
+    {
+        var tex = (Texture2D)Selection.activeObject;
+        if (tex == null) return;
+        var pngData = tex.EncodeToPNG();
+        System.IO.File.WriteAllBytes(Application.dataPath + "/tex.png", pngData);
+    }
+
+#endif
+}
--- Readme.md
+++ Readme.md
@@ -1,43 +1,7 @@
-# Animation Texture Baker for Unity
+# Installation 
 
-## Description
-
-- Bake vertex of Animation(legacy) to Texture2D
-- Bake data of Positions and Normals
-
-![](imgs/original.png)
-
-- Original GameObject has Animation Component, SkinnedMeshRenderer, Bones.
-- Convert Animation-Data to Texture2D.
-
-![](imgs/positionsTex.png)
-data of Positions.
-
-![](imgs/normalsTex.png)
-data of Normals.
-
-![](imgs/converted.png)
-Converted, has only one GameObject.
-
-![](imgs/animated.gif)
-running.
-
-## Animation Texture Baker
-
-![](imgs/baker.png)
-
-- C\# script(for setup)
-- ComputeShader(Texture Generator)
-- Shader(Texture Animation Player)
-
-## Shader Graph
-
-![](imgs/umaGraph.gif)
-
-- Assets/ShaderGraph
-- Need to edit the mesh at runtime..
-  - Vertex index data is being copied to mesh.uv1 by script
-
-## Horse
-
-horse is from Unity [AssetStore](https://www.assetstore.unity3d.com/jp/#!/content/16687). big up
+Edit the `manifest.json` file located in the `Packages` folder of your unity project and
+add the follwing line to the list of `dependencies`:
+```json
+"com.nukadelic.animationtexturebaker": "https://github.com/nukadelic/AnimationTextureBaker.git"
+```
